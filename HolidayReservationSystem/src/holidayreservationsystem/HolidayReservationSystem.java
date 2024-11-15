2eea2de2381b62ddbc9f56899be6bff5a9dc1c41
Castelle Low Tian Yee <e1138776@u.nus.edu>
2024-11-15 10:45:05 +0800

    login for client

---
+++ HolidayReservationSystem/src/holidayreservationsystem/HolidayReservationSystem.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package holidayreservationsystem;
+
+import ws.partner.InvalidLoginCredentialException_Exception;
+import ws.partner.PartnerWebService_Service;
+
+/**
+ *
+ * @author castellelow
+ */
+public class HolidayReservationSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws InvalidLoginCredentialException_Exception {
+        // TODO code application logic here
+        MainApp mainApp = new MainApp();
+        mainApp.runApp();
+    }
+    
+}
